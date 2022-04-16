8f0aa385f19cadbedef7d4f0f4e16310c3cec1bb
jakenorris1124 <jakenorris1124@gmail.com>
2022-04-16 16:58:29 -0400

    Added the ability to place objects from the inventory. Clicking on an object in the inventory will make it turn green. Clicking again in the world will place that object down and change it to red in the inventory. Clicking in an invalid spot won't place the object. Clicking an already placed object in the inventory won't place it.

--- src/UI.js
+++ src/UI.js
@@ -1,3 +1,8 @@
+const MIN_X = 0
+const MAX_X = 1600
+const MIN_Y = 0
+const MAX_Y = 1080
+
 export default class UI
 {
     /**
@@ -10,6 +15,13 @@ export default class UI
         this.scene = scene;
         this.level = level;
         this.machines = machines;
+
+        this.machineStatus = [];
+        for (let i in this.machines)
+        {
+            this.machineStatus.push(true);
+        }
+
         this.drawUI();
     }
 
@@ -18,12 +30,17 @@ export default class UI
         var sidebar = this.scene.add.rectangle(1800, 540, 400, 1080, 0x666666); //Draws the sidebar for the UI
         this.scene.add.text(1610, 10, this.level, {fill: '#ffffff'}); //Used to help identify the current level
 
+
         //For loop that prints the names of all the objects available (later will show the objects themselves)
+        var machineButtons = [];
         for (let i in this.machines)
         {
-            this.scene.add.text(1610, 30+ (i * 20), this.machines[i], {fill: '#ffffff'});
+            machineButtons.push(this.scene.add.text(1610, 30+ (i * 20), this.machines[i], {fill: '#ffffff'}));
+            machineButtons[i].setInteractive();
+            machineButtons[i].on('pointerdown', () => this.place(i, machineButtons[i]));
         }
 
+        //Button to return to main menu
         const returnToMain = this.scene.add.text(1610, 1050, 'Back to Main Menu', {fill: '#ffffff'});
         returnToMain.setInteractive();
         returnToMain.on('pointerdown', () => {
@@ -43,5 +60,33 @@ export default class UI
 
         this.scene.add.text(800, 450, "You Win!", {fill: '#ffffff'});
     }
+
+    place(index, button)
+    {
+        if (!this.machineStatus[index])
+            return;
+        button.setColor("#31ff00");
+
+        setTimeout(() => {
+            this.scene.input.once('pointerdown', (pointer) => {
+                if(this.inBounds(pointer.x, pointer.y))
+                {
+                    button.setColor("#ff0000");
+                    this.scene.add.text(pointer.x, pointer.y, this.machines[index], {fill: '#ffffff'})
+                    this.machineStatus[index] = false;
+                }
+            }), 10
+        });
+
+
+    }
+
+    inBounds(x, y)
+    {
+        if(x >= MIN_X && x<= MAX_X && y >= MIN_Y && y<= MAX_Y)
+            return true;
+        return false;
+    }
+
 
 }
