d85b34bf92d0ad5880c196054c2a33e8e40eb7d9
Aaron <AaronSchwenke@knights.ucf.edu>
2022-04-18 17:44:07 -0400

    Adjusted the game to fit the screen (still a has a minor scroll for vertical space for some reason.) and centered it. You guys should test that it is readjusted and centered on your screens as well.

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ const config = {
 	type: Phaser.AUTO,
 	width: 1920,
 	height: 1080,
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH
+	},
 	physics: {
 		default: 'arcade',
 		arcade: {
--- src/puzzleObjects/Buttons.js
+++ src/puzzleObjects/Buttons.js
@@ -12,7 +12,6 @@ export default class Buttons
     }
 
     /**
-     *
      * @param {Phaser.Physics.Arcade.StaticBody} piece Object that the button will be linked to.
      */
     place(x = 0, y = 0, piece)
