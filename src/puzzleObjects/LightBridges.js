7a522a53bad563a0e8af3b8d6ba89674546f6533
jakenorris1124 <jakenorris1124@gmail.com>
2022-04-20 20:54:22 -0400

    Added rotation functionality to the Light Bridges

--- src/puzzleObjects/LightBridges.js
+++ src/puzzleObjects/LightBridges.js
@@ -44,6 +44,45 @@ export default class LightBridges
             return;
         }
 
+        var angle = emitter.angle
+        let emitterX;
+        let emitterY;
+        let beamWidth;
+        let beamHeight;
+        switch (angle)
+        {
+            case 0:
+                emitterX = emitter.body.position.x+125
+                emitterY = emitter.body.position.y+55
+                beamWidth = 20
+                beamHeight = 35
+                break
+            case 90:
+                emitterX = emitter.body.position.x+55
+                emitterY = emitter.body.position.y+125
+                beamWidth = 35
+                beamHeight = 20
+                break
+            case 180:
+                emitterX = emitter.body.position.x
+                emitterY = emitter.body.position.y+55
+                beamWidth = 20
+                beamHeight = 35
+                break
+            case -180:
+                emitterX = emitter.body.position.x
+                emitterY = emitter.body.position.y+55
+                beamWidth = 20
+                beamHeight = 35
+                break
+            case -90:
+                emitterX = emitter.body.position.x+55
+                emitterY = emitter.body.position.y
+                beamWidth = 35
+                beamHeight = 20
+                break
+        }
+
         /*
             Create a small rectangle called 'beam', and set its gravity to be very high in the
             x direction so that it looks like it is getting shot out very fast. This is the little red box that gets shot out from the lightBridge emitter.
@@ -51,10 +90,34 @@ export default class LightBridges
             Once the collision occurs, the distance between where the collision occurred and where the beam is shot out from is calculated and saved as 'length'.
             A rectangle called 'bridge' is made that is 'length' long. Bridge is the actual bridge that gets emitted.
          */
-        let beam = this.scene.add.rectangle(emitter.body.position.x+125, emitter.body.position.y+55, 20, 35, 0xff0000)
+        let beam = this.scene.add.rectangle(emitterX, emitterY, beamWidth, beamHeight, 0xff0000)
         this.scene.physics.add.existing(beam)
-        beam.body.setGravityY(-200)
-        beam.body.setGravityX(10000)
+
+
+        switch (angle)
+        {
+            case 0:
+                beam.body.setGravityY(-200)
+                beam.body.setGravityX(10000)
+                break
+            case 90:
+                beam.body.setGravityY(10200)
+                beam.body.setGravityX(0)
+                break
+            case 180:
+                beam.body.setGravityY(-200)
+                beam.body.setGravityX(-10000)
+                break
+            case -180:
+                beam.body.setGravityY(-200)
+                beam.body.setGravityX(-10000)
+                break
+            case -90:
+                beam.body.setGravityY(-10200)
+                beam.body.setGravityX(0)
+                break
+        }
+
 
         let length;
 
@@ -63,12 +126,32 @@ export default class LightBridges
             this.scene.fanGroup, this.scene.gravityInverterGroup, this.scene.heavyBallTransformerGroup, this.scene.lightBallTransformerGroup, this.scene.pullyGroup,
             this.scene.sprinGroup, this.scene.HBTBoundaryGroup, this.scene.LBTBoundaryGroup], () => {
                 //Calculate length then destroy beams
-                length = Math.abs(beam.body.position.x - (emitter.body.position.x+100))
-                beam.destroy()
+
+
 
                 //Create bridge then add physical properties to it
-                emitter.setData('bridge', this.scene.add.rectangle(emitter.body.position.x+ (length/2) +125, emitter.body.position.y+55, length, 35, 0xf700ff))
+                switch (angle)
+                {
+                    case 0:
+                        length = Math.abs(beam.body.position.x - (emitter.body.position.x+100))
+                        emitter.setData('bridge', this.scene.add.rectangle(emitterX + (length/2),emitterY, length, 35, 0xf700ff))
+                        break
+                    case 90:
+                        length = Math.abs(beam.body.position.y - (emitter.body.position.y+100))
+                        emitter.setData('bridge', this.scene.add.rectangle(emitterX,emitterY + (length/2), 35, length, 0xf700ff))
+                        break
+                    case 180:
+                    case -180:
+                        length = Math.abs(beam.body.position.x - (emitter.body.position.x))
+                        emitter.setData('bridge', this.scene.add.rectangle(emitterX - (length/2),emitterY, length, 35, 0xf700ff))
+                        break
+                    case -90:
+                        length = Math.abs(beam.body.position.y - (emitter.body.position.y))
+                        emitter.setData('bridge', this.scene.add.rectangle(emitterX,emitterY - (length/2), 35, length, 0xf700ff))
+                        break
+                }
 
+                beam.destroy()
                 this.scene.physics.add.existing(emitter.getData('bridge'))
                 emitter.getData('bridge').body.immovable = true;
                 emitter.getData('bridge').body.allowGravity = false;
