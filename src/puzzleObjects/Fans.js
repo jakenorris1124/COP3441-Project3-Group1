87c8b8188e39935dc9483155ba6a4d113d73b6d2
DAntaeAronne <94494773+DAntaeAronne@users.noreply.github.com>
2022-04-22 00:18:20 -0400

    Maybe figured out animation. Testing

--- src/puzzleObjects/Fans.js
+++ src/puzzleObjects/Fans.js
@@ -21,6 +21,88 @@ export default class Fans
         this._windGroup = this.scene.physics.add.staticGroup()
     }
 
+    // Creates sprite for the fan
+    createSprite(){
+
+        // Create 'On' animation
+        this.scene.anims.create({
+            key: 'On',
+            frames: [
+                {
+                    key: 'fan',
+                    frame: '0000.png'
+                },{
+                    key: 'fan',
+                    frame: '0001.png'
+                },{
+                    key: 'fan',
+                    frame: '0002.png'
+                },{
+                    key: 'fan',
+                    frame: '0003.png'
+                },{
+                    key: 'fan',
+                    frame: '0004.png'
+                },{
+                    key: 'fan',
+                    frame: '0005.png'
+                },{
+                    key: 'fan',
+                    frame: '0006.png'
+                },{
+                    key: 'fan',
+                    frame: '0007.png'
+                }],
+            skipMissedFrames: true,
+            defaultTextureKey: null,
+            startFrame: 0,
+
+            // time
+            delay: 0,
+            frameRate: null,
+            duration: null,
+            timeScale: 1,
+
+            // repeat
+            repeat: -1,              // set to (-1) to repeat forever
+            repeatDelay: 0,
+            yoyo: false,
+
+            // visible
+            showOnStart: false,
+            hideOnComplete: false
+        }) // end of create 'On' animation;
+
+        // Create 'Off' animation
+        this.scene.anims.create({
+            key: 'Off',
+            frames: [
+                {
+                    key: 'fan',
+                    frame: '0008.png'
+                }],
+            skipMissedFrames: true,
+            defaultTextureKey: null,
+            startFrame: 0,
+
+            // time
+            delay: 0,
+            frameRate: null,
+            duration: null,
+            timeScale: 1,
+
+            // repeat
+            repeat: 0,              // set to (-1) to repeat forever
+            repeatDelay: 0,
+            yoyo: false,
+
+            // visible
+            showOnStart: false,
+            hideOnComplete: false
+        }) // end of create 'Off' animation;
+
+    }
+
     /**
      * @returns {Phaser.Physics.Arcade.StaticGroup}
      */
@@ -46,10 +128,13 @@ export default class Fans
         const wind = fan.getData('wind')
 
         if (wind.state == ON) {
+            fan.play('Off')
             wind.setState(OFF)
         }
-        else
+        else {
+            fan.play('On')
             wind.setState(ON)
+        }
     }
 
     /**
@@ -73,7 +158,10 @@ export default class Fans
      */
     place(x = 0, y = 0)
     {
+        this.scene.load.atlas('fan','images/Fan.png','images/Fan.json')
         const fan = this.scene.add.sprite(x, y, this.key)
+        this.createSprite()
+        fan.play('Off')
         this._group.add(fan)
         const wind = this.windGroup.create(x, y - fan.body.height - 150, this.windKey)
         wind.visible = false
--- src/puzzleObjects/Prisms.js
+++ src/puzzleObjects/Prisms.js
@@ -13,6 +13,158 @@ export default class Prisms
         this._group = this.scene.physics.add.staticGroup()
     }
 
+    // Creates sprite for the fan
+    createSprite(){
+        this.scene.load.atlas('prism','images/Prism.png','images/Prism.json');
+
+        // Create 'On' animation
+        this.scene.anims.create({
+            key: 'On',
+            frames: [
+                {
+                    key: 'prism',
+                    frame: '0000.png'
+                },{
+                    key: 'prism',
+                    frame: '0001.png'
+                },{
+                    key: 'prism',
+                    frame: '0002.png'
+                },{
+                    key: 'prism',
+                    frame: '0004.png'
+                },{
+                    key: 'prism',
+                    frame: '0005.png'
+                },{
+                    key: 'prism',
+                    frame: '0006.png'
+                },{
+                    key: 'prism',
+                    frame: '0007.png'
+                },{
+                    key: 'prism',
+                    frame: '0008.png'
+                },{
+                    key: 'prism',
+                    frame: '0009.png'
+                },{
+                    key: 'prism',
+                    frame: '0010.png'
+                },{
+                    key: 'prism',
+                    frame: '0011.png'
+                },{
+                    key: 'prism',
+                    frame: '0012.png'
+                },{
+                    key: 'prism',
+                    frame: '0013.png'
+                },{
+                    key: 'prism',
+                    frame: '0014.png'
+                },{
+                    key: 'prism',
+                    frame: '0015.png'
+                }],
+            skipMissedFrames: true,
+            defaultTextureKey: null,
+            startFrame: 0,
+
+            // time
+            delay: 0,
+            frameRate: null,
+            duration: null,
+            timeScale: 1,
+
+            // repeat
+            repeat: -1,              // set to (-1) to repeat forever
+            repeatDelay: 0,
+            yoyo: false,
+
+            // visible
+            showOnStart: false,
+            hideOnComplete: false
+        }) // end of create 'On' animation;
+
+        // Create 'Off' animation
+        this.scene.anims.create({
+            key: 'Off',
+            frames: [
+                {
+                    key: 'prism',
+                    frame: '0016.png'
+                },{
+                    key: 'prism',
+                    frame: '0017.png'
+                },{
+                    key: 'prism',
+                    frame: '0018.png'
+                },{
+                    key: 'prism',
+                    frame: '0019.png'
+                },{
+                    key: 'prism',
+                    frame: '0020.png'
+                },{
+                    key: 'prism',
+                    frame: '0021.png'
+                },{
+                    key: 'prism',
+                    frame: '0022.png'
+                },{
+                    key: 'prism',
+                    frame: '0023.png'
+                },{
+                    key: 'prism',
+                    frame: '0024.png'
+                },{
+                    key: 'prism',
+                    frame: '0025.png'
+                },{
+                    key: 'prism',
+                    frame: '0026.png'
+                },{
+                    key: 'prism',
+                    frame: '0027.png'
+                },{
+                    key: 'prism',
+                    frame: '0028.png'
+                },{
+                    key: 'prism',
+                    frame: '0029.png'
+                },{
+                    key: 'prism',
+                    frame: '0030.png'
+                },{
+                    key: 'prism',
+                    frame: '0031.png'
+                },{
+                    key: 'prism',
+                    frame: '0032.png'
+                }],
+            skipMissedFrames: true,
+            defaultTextureKey: null,
+            startFrame: 0,
+
+            // time
+            delay: 0,
+            frameRate: null,
+            duration: null,
+            timeScale: 1,
+
+            // repeat
+            repeat: 0,              // set to (-1) to repeat forever
+            repeatDelay: 0,
+            yoyo: false,
+
+            // visible
+            showOnStart: false,
+            hideOnComplete: false
+        }) // end of create 'Off' animation;
+
+    }
+
     /**
      * @param {number} x
      * @param {number} y
@@ -20,7 +172,9 @@ export default class Prisms
      */
     place(x = 0, y = 0)
     {
+        this.createSprite()
         const prism = this.scene.add.sprite(x, y, this.key)
+        prism.play('Off')
         this._group.add(prism)
 
         return prism
