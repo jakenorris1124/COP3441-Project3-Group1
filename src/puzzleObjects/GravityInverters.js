634010483e3634c646a2768a7f360b5d327ab952
DAntaeAronne <94494773+DAntaeAronne@users.noreply.github.com>
2022-04-22 21:51:31 -0400

    Grav Inv and All animation finished

--- src/puzzleObjects/GravityInverters.js
+++ src/puzzleObjects/GravityInverters.js
@@ -25,6 +25,42 @@ export default class GravityInverters
         return this._group
     }
 
+    createSprite(){
+        // Create 'Off' animation
+        this.gravityInverter.anims.create({
+            key: 'Off',
+            frames: [{key: this.key, frame: '0000.png'
+            }],
+            frameRate: 20,
+            repeat: 0              // set to (-1) to repeat forever
+        }); // end of create 'Off' animation
+
+        // Create 'On' animation
+        this.gravityInverter.anims.create({
+            key: 'On',
+            frames: [{key: this.key, frame: '0017.png'
+            }],
+            frameRate: 20,
+            repeat: 0              // set to (-1) to repeat forever
+        }); // end of create 'On' animation
+
+        // Create 'Act' animation
+        this.gravityInverter.anims.create({
+            key: 'Act',
+            frames: this.gravityInverter.anims.generateFrameNames(this.key, {start: 0, end: 17, zeroPad: 4, prefix: "", suffix: ".png"}),
+            frameRate: 100,
+            repeat: 0              // set to (-1) to repeat forever
+        }); // end of create 'Act' animation
+
+        // Create 'DeAct' animation
+        this.gravityInverter.anims.create({
+            key: 'DeAct',
+            frames: this.gravityInverter.anims.generateFrameNames(this.key, {start: 18, end: 35, zeroPad: 4, prefix: "", suffix: ".png"}),
+            frameRate: 100,
+            repeat: 0              // set to (-1) to repeat forever
+        }); // end of create 'DeAct' animation
+    }
+
     /**
      * @param {number}x
      * @param {number} y
@@ -32,12 +68,14 @@ export default class GravityInverters
      */
     place(x = 0, y = 0)
     {
-        const gravityInverter = this.scene.add.sprite(x, y, this.key)
-        this._group.add(gravityInverter)
+        this.gravityInverter = this.scene.add.sprite(x, y, this.key)
+        this.createSprite()
+        this.gravityInverter.play('Off')
+        this._group.add(this.gravityInverter)
 
-        gravityInverter.setState(OFF)
+        this.gravityInverter.setState(OFF)
 
-        return gravityInverter
+        return this.gravityInverter
     }
 
     // Inverts gravity from its current state.
@@ -52,11 +90,13 @@ export default class GravityInverters
 
         if(gravityInverter.state == OFF)
         {
+            this.gravityInverter.play('Act')
             ball.body.setGravityY(-400 - ball.body.gravity.y)
             gravityInverter.setState(ON)
         }
         else
         {
+            this.gravityInverter.play('DeAct')
             ball.body.setGravityY(0 - ball.body.gravity.y)
             gravityInverter.setState(OFF)
         }
